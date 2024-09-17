d0edf4b048480908bfbb15965a07979bca3e1052
Holly <holly.jukes@yahoo.co.uk>
2024-09-17 12:09:29 +0100

    added name

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.printIn(sayHelloToSomeone("Simon"));
+        System.out.println(sayHelloToSomeone("Simon"));
         System.out.println(sayGoodbye());
     }
 
